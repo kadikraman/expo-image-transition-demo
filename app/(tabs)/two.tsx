817d8b984d041103b7a7d84804831cf7a8ba7f81
Kadi Kraman <kadi@expo.io>
2024-02-02 15:20:17 +0000

    Add image demo

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -2,12 +2,19 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Image } from 'expo-image';
 
 export default function TabTwoScreen() {
+  console.log("TabTwoScreen RENDER")
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Image
+        source={{ uri: 'https://images.unsplash.com/photo-1706170498499-a12be93c842e?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
+        style={{ height: 100, width: 100}}
+        transition={{ effect: 'flip-from-right', duration: 250 }}
+      />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
--- app/modal.tsx
+++ app/modal.tsx
@@ -3,14 +3,20 @@ import { Platform, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Image } from 'expo-image';
 
 export default function ModalScreen() {
+  console.log("ModalScreen RENDER")
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/modal.tsx" />
-
+      <Image
+        source={{ uri: 'https://images.unsplash.com/photo-1706170498499-a12be93c842e?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
+        style={{ height: 100, width: 100}}
+        transition={{ effect: 'flip-from-right', duration: 250 }}
+      />
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
